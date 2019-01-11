c817baf1208ce6c2bc3005c0c95e216a23c76947
fongp9625 <38332926+fongp9625@users.noreply.github.com>
2019-01-11 10:20:49 -0800

    Add project files.

---
+++ MeetingRegistration/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MeetingRegistration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
